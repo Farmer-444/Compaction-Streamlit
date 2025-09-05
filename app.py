f55b1c700ac1b0d21de9e0f23940408b9693a43d
Farmer-444 <andrewfraserag@gmail.com>
2025-09-05 14:32:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from typing import List, Optional, Tuple
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
 import plotly.graph_objects as go
 
